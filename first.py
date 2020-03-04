65363e5e5a50dc2a74a7a7b4ef813e6b66e0d621
Iqbal Siddiqui <iqbal.siddiqui@gmail.com>
2020-03-04 14:16:49 +0000

    Adding new project files

---
+++ first.py
@@ -0,0 +1,3 @@
+strName = input("What is your name = ")
+strAge = input("What is your age = ")
+print("Hello " , strName, ", you are", strAge, "years old.")  
---
+++ function - 1.py
@@ -0,0 +1,9 @@
+def isOddOrEven (inputNumber):
+    if int(inputNumber)%2==0:
+        print("You entered an even number!")
+    else:
+        print("You entered an odd number")
+
+myNumber = input("Enter any number = ")
+print(isOddOrEven(myNumber))
+    
